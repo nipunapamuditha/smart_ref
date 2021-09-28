40c0b66d13f9dd7b6d4f369769c5c54a78e6c46c
nipunapamuditha <nipunapamuditha234@gmail.com>
2021-09-28 11:04:52 +0530

    2

--- app/src/main/java/com/example/smart_refrigirator/addfoodactivity.java
+++ app/src/main/java/com/example/smart_refrigirator/addfoodactivity.java
@@ -1,16 +1,30 @@
 package com.example.smart_refrigirator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
+
 
 public class addfoodactivity extends AppCompatActivity {
 
@@ -18,6 +32,7 @@ public class addfoodactivity extends AppCompatActivity {
     private ImageView imageView;
     private TextView textView;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +48,22 @@ public class addfoodactivity extends AppCompatActivity {
         capture_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                dispatchTakePictureIntent();
+                textView.setText("");
             }
+
+        });
+
+        detact_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detactTextFromImmage();
+            }
+
         });
     }
+
+
 
 
     private void dispatchTakePictureIntent() {
@@ -52,8 +79,42 @@ public class addfoodactivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    private void detactTextFromImmage() {
+
+        FirebaseVisionImage firebaseVisionImage=FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector = FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextfromImage(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(addfoodactivity.this,"error " +e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("error" , e.getMessage());
+            }
+        });
+    }
+
+    private void displayTextfromImage(FirebaseVisionText firebaseVisionText) {
+
+        List<FirebaseVisionText.Block> blockList = firebaseVisionText.getBlocks();
+        if(blockList.size() == 0)
+        {
+            Toast.makeText(this,"No text from image",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for(FirebaseVisionText.Block block : firebaseVisionText.getBlocks())
+            {
+                String text = block.getText();
+                textView.setText(text);
+            }
         }
     }
 }
