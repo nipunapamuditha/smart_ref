9e9cdcb7278f45d5c9dcf1f637e9bc8b4aa4b632
nipunapamuditha <nipunapamuditha234@gmail.com>
2021-10-05 11:27:18 +0530

    last

--- app/src/main/java/com/example/smart_refrigirator/addfoodactivity.java
+++ app/src/main/java/com/example/smart_refrigirator/addfoodactivity.java
@@ -12,43 +12,78 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-
+import java.util.Date;
 import java.util.List;
 
 
 public class addfoodactivity extends AppCompatActivity {
 
-    private Button capture_image,detact_image;
-    private ImageView imageView;
-    private TextView textView;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    Bitmap imageBitmap;
+EditText food_name;
+EditText food_exp;
+RadioButton tr1,tr2,tr3,tr4;
+Button add;
+Date exp;
+    CalendarView simpleCalendarView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addfoodactivity);
 
+        food_name= findViewById(R.id.EnterName);
+
+        tr1= findViewById(R.id.radioButton);
+        tr2= findViewById(R.id.radioButton2);
+        tr3= findViewById(R.id.radioButton3);
+        tr4= findViewById(R.id.radioButton4);
+        add= findViewById(R.id.button_add);
+
+        // getting from calendar .
+         simpleCalendarView = (CalendarView) findViewById(R.id.exp_calander);
 
 
-        capture_image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                textView.setText("");
-            }
 
-        });
 
 
     }
 
+    public void add_to_database()
+    {
+        int trayno=0;
 
+        if(tr1.isChecked())
+        {
+            trayno=1;
+        }
+        if(tr2.isChecked())
+        {
+            trayno=2;
+        }
+        if(tr3.isChecked())
+        {
+            trayno=3;
+        }
+        if(tr4.isChecked())
+        {
+            trayno=4;
+        }
+
+        String f_name = food_name.getText().toString();
+
+        long selectedDate = simpleCalendarView.getDate();
+        exp = new Date(selectedDate);
+    }
 
 
 
